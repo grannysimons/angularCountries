4540c7a20512e2de0232fc7cc3e93ca0868dd956
Mariona Roca <mroca@profile.local>
2018-12-07 12:48:07 +0100

    contry links

--- paises/src/app/app-routing.module.ts
+++ paises/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CountriesComponent } from './countries/countries.component';
+import { CountryComponent } from './country/country.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
